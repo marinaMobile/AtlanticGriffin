60c5f89842f8cfeea9a09d0ddeaecba2896e0bd4
Marina <marina@Marinas-MacBook-Pro.local>
2022-11-03 12:54:43 +0300

    first commit

---
+++ app/src/main/java/com/creativemobile/z/black/CNST.kt
@@ -0,0 +1,18 @@
+package com.creativemobile.z.black
+
+object CNST {
+    const val AF_DEV_KEY = "8TtvzVu8gABwCgyR8YLktG"
+    const val jsoupCheck = "3g4f"
+    const val ONESIGNAL_APP_ID = "530e0394-5e4d-4bb0-b87f-ba9b671601fb"
+
+    var lru = "http://atlanticgriffin.xyz/go.php?to=1&"
+
+
+    val odone = "sub_id_1="
+    val twoSub = "sub_id_2="
+
+    var MAIN_ID: String? = ""
+    var C1: String? = "c11"
+    var D1: String? = "d11"
+    var DEV: String = "false"
+}
